96095630d9d670183e614019fbd88fd27c83d1f5
Jose Alvarez <jose.alvarez@falconeye.local>
2020-09-15 13:59:27 -0400

    docs added

---
+++ docs/precache-manifest.ccc7aba6ef4f3a4a20fb6d73e3af304b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ac45c7c97f60f3f28326a6177f346f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4654d397c08c7c5f753d",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4654d397c08c7c5f753d",
+    "url": "/static/js/main.d2e222f4.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
